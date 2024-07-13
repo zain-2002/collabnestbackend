822ea5e0b4539f60a3996634195afb8dc7fd779a
ZAIN UL ABIDEEN <100013909+zain-2002@users.noreply.github.com>
2024-07-13 12:04:08 +0500

    changes

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.post('/create-payment-intent', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`API is running on port ${PORT}`));
